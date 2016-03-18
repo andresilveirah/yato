45587ba5c77268bbeb37522abb26cfe378245ae3
Andre Herculano <aherculano@babbel.com>
2016-03-18 17:47:29 +0100

    Updates

--- test/setup.js
+++
@@ -1,8 +0,0 @@
-import { jsdom } from 'jsdom';
-import expect from 'expect.js';
-
-global.document = jsdom('<!doctype html><html><body></body></html>');
-global.window = document.defaultView;
-global.navigator = global.window.navigator;
-
-global.expect = expect;
