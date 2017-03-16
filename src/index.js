02a6d8272679a7af9459a55f8192abb4561c6c91
Andre Herculano <aherculano@babbel.com>
2017-03-16 16:34:57 +0100

    throttle the amount of times we persist the state

--- src/index.js
+++ src/index.js
@@ -8,12 +8,18 @@ import TodoApp from './components/TodoApp';
 
 import { loadState, persistState } from './localStorage';
 
+import throttle from 'lodash/throttle';
+
+const PERSIST_STATE_INTERVAL = 1000;
+
 // creates the store with the reducers (result of combineReducers)
 // the second parameter is simply an object containing the pre-state of the app
 const store = createStore(reducers, loadState());
 
-// on every store change we persist the state
-store.subscribe(() => persistState(store.getState()));
+// on every store change we persist the state, at most once a second (throtle function)
+store.subscribe(throttle(() => {
+  persistState(store.getState());
+}, PERSIST_STATE_INTERVAL));
 
 ReactDOM.render(
   <Provider store={store}>
