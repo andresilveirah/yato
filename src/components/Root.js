45db5667c51dd89859e9ecb4449cf2cc2f65a013
Andre Herculano <aherculano@babbel.com>
2017-03-20 12:52:13 +0100

    add Router to Root component

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import TodoApp from './TodoApp';
 
 const Root = ({ store }) => (
@@ -8,7 +9,9 @@ const Root = ({ store }) => (
       <h1>Yet another TODO app</h1>
     </header>
     <Provider store={store}>
-      <TodoApp />
+      <Router>
+        <Route path='/' component={TodoApp} />
+      </Router>
     </Provider>
   </div>
 );
