a3bf462be798bbcab3b7360b99289b39c44b00e3
Andre Herculano <aherculano@babbel.com>
2017-03-23 17:58:34 +0100

    remove dependency on crypto package

--- src/services/uuid.js
+++ src/services/uuid.js
@@ -1,6 +1,5 @@
 // A quick and dirty replacement for node-uuid package
 // source https://gist.github.com/jed/982883
 
-import crypto from 'crypto';
-
-export const uuid = a => a?(a^crypto.randomBytes(1)[0] % 16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,uuid);
+// eslint-disable-next-line
+export const uuid = a => a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,uuid);
