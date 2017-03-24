1364f043cc27cb845b5dd96f384b68f04cc9fbaa
Andre Herculano <aherculano@babbel.com>
2017-03-24 16:30:10 +0100

    move getVisibleTodos to todos reducer

--- src/containers/VisibleTodosList.js
+++ src/containers/VisibleTodosList.js
@@ -7,22 +7,10 @@ import { withRouter } from 'react-router-dom';
 
 import TodosList from '../components/TodosList';
 import { toggleTodo } from '../actions/index';
-
-const getVisibleTodos = (todos, filter) => {
-  switch (filter) {
-    case 'all':
-      return todos;
-    case 'completed':
-      return todos.filter(t => t.completed);
-    case 'incompleted':
-      return todos.filter(t => !t.completed);
-    default:
-      return todos;
-  }
-};
+import { getVisibleTodos } from '../reducers';
 
 const mapTodosStateToProps = (state, { match }) => ({
-  todos: getVisibleTodos(state.todos, match.params.filter || 'all')
+  todos: getVisibleTodos(state, match.params.filter || 'all')
 });
 
 /**
