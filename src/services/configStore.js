4fdf3f7ccbf936930a29148fa32e12c4fcf46216
André Herculano <andresilveirah@gmail.com>
2017-04-02 19:00:40 +0200

    remove localStorage service

--- src/services/configStore.js
+++ src/services/configStore.js
@@ -1,10 +1,5 @@
 import { createStore } from 'redux';
-import throttle from 'lodash/throttle';
-
 import reducers from '../reducers/';
-import { loadState, persistState } from './localStorage';
-
-const PERSIST_STATE_INTERVAL = 1000;
 
 const addLogginToActions = (store) => {
   if (!console.group) return store.dispatch;
@@ -22,20 +17,13 @@ const addLogginToActions = (store) => {
 };
 
 const configStore = () => {
-  // creates the store with the reducers (result of combineReducers)
-  // the second parameter is simply an object containing the pre-state of the app
-  const store = createStore(reducers, loadState());
+  const store = createStore(reducers);
 
   // CATCHA: create-react-app only exposes env variables which are prefixed with REACT_APP_
   // this is to avoid security issues e.g. exposing sensible env variables in the front end
   if(process.env.REACT_APP_DEBUG === 'true') {
     store.dispatch = addLogginToActions(store);
   }
-
-  // on every store change we persist the state, at most once a second (throtle function)
-  store.subscribe(throttle(() => {
-    persistState(store.getState());
-  }, PERSIST_STATE_INTERVAL));
 
   return store;
 };
