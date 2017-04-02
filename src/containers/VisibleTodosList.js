d9fbd1ede6417d324b67f2626ebcd7f522b5cd0e
André Herculano <andresilveirah@gmail.com>
2017-04-02 19:01:10 +0200

    fetch todos on VisibleTodosList component

--- src/containers/VisibleTodosList.js
+++ src/containers/VisibleTodosList.js
@@ -1,30 +1,43 @@
-/**
- * @module containers/VisibleTodosList
- */
-
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 import TodosList from '../components/TodosList';
 import { toggleTodo } from '../actions/index';
 import { getVisibleTodos } from '../reducers';
+import { fetchTodos } from '../services/api';
 
-const mapTodosStateToProps = (state, { match }) => ({
-  todos: getVisibleTodos(state, match.params.filter || 'all')
-});
+class VisibleTodosList extends Component {
+  componentDidMount() {
+    this.fetchData();
+  }
 
-/**
- * Generates a container component called VisibleTodosList using the presentation
- * component {@link module:components/TodosList}
- * @param  {Object} mapTodosStateToProps a map between the Container own
- * state to the presentation props.
- * @param  {Object} mapTodosDispatchToProps a map between the store
- * dispatch state to the presentation props.
- * @return {Object} the VisibleTodosList container component
- */
-const VisibleTodosList = withRouter(connect(
+  componentDidUpdate(prevProps) {
+    if(prevProps.filter !== this.props.filter) {
+      this.fetchData();
+    }
+  }
+
+  fetchData() {
+    fetchTodos(this.props.filter).then((todos) => console.log(todos));
+  }
+
+  render() {
+    return <TodosList {...this.props} />;
+  }
+}
+
+const mapTodosStateToProps = (state, { match }) => {
+  const filter = match.params.filter || 'all';
+  return {
+    todos: getVisibleTodos(state, filter),
+    filter
+  };
+};
+
+VisibleTodosList = withRouter(connect(
   mapTodosStateToProps,
   { onTodoClick: toggleTodo }
-)(TodosList));
+)(VisibleTodosList));
 
 export default VisibleTodosList;
