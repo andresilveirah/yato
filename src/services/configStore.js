36904865c8c866f505b33173baa445f3bccc4744
André Herculano <andresilveirah@gmail.com>
2017-04-02 19:51:00 +0200

    add manual middleware implementation

--- src/services/configStore.js
+++ src/services/configStore.js
@@ -1,42 +1,44 @@
 import { createStore } from 'redux';
 import reducers from '../reducers/';
 
-const addLogginToActions = (store) => {
-  if (!console.group) return store.dispatch;
+const logger = (store) => (next) => {
+  if (!console.group) return next;
 
-  const rawDispatch = store.dispatch;
   return (action) => {
     console.group(action.type);
     console.log('%c prev state', 'color: grey', store.getState());
     console.log('%c action', 'color: blue', action);
-    const rawValue = rawDispatch(action);
+    const rawValue = next(action);
     console.log('%c current state', 'color: green', store.getState());
     console.groupEnd(action.type);
     return rawValue;
   };
 };
 
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-
-  return (action) => {
-    if(typeof action.then === 'function') {
-      return action.then(rawDispatch);
-    }
-    return rawDispatch(action);
+const promise = (store) => (next) => (action) => {
+  if(typeof action.then === 'function') {
+    return action.then(next);
   }
-}
+  return next(action);
+};
+
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+  middlewares.slice().reverse().forEach(middleware => {
+    store.dispatch = middleware(store)(store.dispatch);
+  });
+};
 
 const configStore = () => {
+  const middlewares = [ promise ];
   const store = createStore(reducers);
 
   // CATCHA: create-react-app only exposes env variables which are prefixed with REACT_APP_
   // this is to avoid security issues e.g. exposing sensible env variables in the front end
   if(process.env.REACT_APP_DEBUG === 'true') {
-    store.dispatch = addLogginToActions(store);
+    middlewares.push(logger);
   }
 
-  store.dispatch = addPromiseSupportToDispatch(store);
+  wrapDispatchWithMiddlewares(store, middlewares);
 
   return store;
 };
