096492a781938a53236c4aca33452e0fdca4ec6c
André Herculano <andresilveirah@gmail.com>
2017-04-23 17:10:34 +0200

    handle RECEIVE_TODOS on todos reducer
    * stop handling ADD_TODOS and TOGGLE_TODOS on todos reducer
    * cache todos response per each filter on todos combined reducer

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,49 +1,67 @@
 import { combineReducers } from 'redux';
-import todo from './todo';
 
-const isNotEmpty = (text) => text.replace(/\s/g,'').length > 0;
-const delegateToTodoReducer = (state, action) => ({...state, [action.id]: todo(state[action.id], action)});
+// const isNotEmpty = (text) => text.replace(/\s/g,'').length > 0;
+// const delegateToTodoReducer = (state, action) => ({...state, [action.id]: todo(state[action.id], action)});
 
 const byId = (state = {}, action) => {
+  let nextState;
   switch (action.type) {
-    case 'ADD_TODO':
-      return isNotEmpty(action.text) ? delegateToTodoReducer(state, action) : state;
-    case 'TOGGLE_TODO':
-      return delegateToTodoReducer(state, action);
+    case 'RECEIVE_TODOS':
+      nextState = { ...state };
+      action.response.forEach(todo => { nextState[todo.id] = todo; });
+      return nextState;
     default:
       return state;
   }
 };
 
 const allIds = (state = [], action) => {
+  if(action.filter !== 'all') { return state; }
+
   switch (action.type) {
-    case 'ADD_TODO':
-      return isNotEmpty(action.text) ? [...state, action.id] : state;
+    case 'RECEIVE_TODOS':
+      return action.response.map(todo => todo.id);
     default:
       return state;
   }
 };
 
+const completedIds = (state = [], action) => {
+  if(action.filter !== 'completed') { return state; }
+
+  switch (action.type) {
+    case 'RECEIVE_TODOS':
+      return action.response.map(todo => todo.id);
+    default:
+      return state;
+  }
+};
+
+const incompletedIds = (state = [], action) => {
+  if(action.filter !== 'incompleted') { return state; }
+
+  switch (action.type) {
+    case 'RECEIVE_TODOS':
+      return action.response.map(todo => todo.id);
+    default:
+      return state;
+  }
+};
+
+const idsByFilter = combineReducers({
+  all: allIds,
+  completed: completedIds,
+  incompleted: incompletedIds
+});
+
 // While the Todo reducer manages a single Todo, this reducer is responsible for the collection of Todos.
 // The state is 'normalized'.
 // byId's state is an object with all todos indexed by its id.
 // allIds' state is an Array containing all Todos' ids.
-export default combineReducers({ byId, allIds });
+export default combineReducers({ byId, idsByFilter });
 
-const getAllTodos = (state) => state.allIds.map(id => state.byId[id]);
-
-// This function works as a selector.
+// This function works as a selector. Filtering todos by the selected filter and
+// mapping to an array of Todos.
 export const getVisibleTodos = (state, filter) => {
-  const allTodos = getAllTodos(state);
-
-  switch (filter) {
-    case 'all':
-      return allTodos;
-    case 'completed':
-      return allTodos.filter(t => t.completed);
-    case 'incompleted':
-      return allTodos.filter(t => !t.completed);
-    default:
-      throw new Error(`Unknown filter ${filter}`);
-  }
+  return state.idsByFilter[filter].map(id => state.byId[id]);
 };
--- src/reducers/todos.spec.js
+++ src/reducers/todos.spec.js
@@ -1,71 +1,18 @@
-import todos, { getVisibleTodos } from './todos';
-import todo from './todo';
-import { addTodo, toggleTodo } from '../actions';
+import { getVisibleTodos } from './todos';
 
 describe('todos reducer', () => {
-  let state = undefined;
+  let state = {};
 
-  describe('when receiving an ADD_TODO action', () => {
-    describe('and the todos text is not empty', () => {
-      let action = addTodo('Hello world');
-
-      it('adds the todos id to its allIds attribute', () => {
-        expect(todos(state, action).allIds).toEqual([action.id]);
-      });
-
-      it('adds the todo to its byId attribute', () => {
-        expect(todos(state, action).byId).toEqual({
-          [action.id]: todo(state, action)
-        });
-      });
-    });
-    describe('and the todos text is empty', () => {
-      let action = addTodo('');
-
-      it('doesn\'t add the todo id to its allIds attribute', () => {
-        expect(todos(state, action).allIds).toEqual([]);
-      });
-
-      it('doesn\'t add the todo to its byId attribute', () => {
-        expect(todos(state, action).byId).toEqual({});
-      });
-    });
-  });
-
-  describe('when receiving a TOGGLE_TODO action', () => {
-    let action = toggleTodo(1);
-
-    it('allIds attribute should not change', () => {
-      let state = { allIds: [1,2,3] };
-
-      expect(todos(state, action).allIds).toEqual(state.allIds);
-    });
-
-    it('byId should delegate the TOGGLE_TODO action to the todo reducer', () => {
-      let state = {
+  describe('getVisibleTodos selector', () => {
+    it('filters all todos', () => {
+      state = {
+        idsByFilter: { all: [1, 2] },
         byId: {
-          "1": { id: 1, text: 'to be toggled', completed: false },
-          "2": { id: 2, text: 'hello world', completed: false }
+          "1": { id: 1, text: 'completed todo', completed: true },
+          "2": { id: 2, text: 'incompleted todo', completed: false }
         }
       };
 
-      expect(todos(state, action).byId).toEqual({
-        "1": { id: 1, text: 'to be toggled', completed: true },
-        "2": { id: 2, text: 'hello world', completed: false }
-      });
-    });
-  });
-
-  describe('getVisibleTodos selector', () => {
-    let state = {
-      allIds: [1,2],
-      byId: {
-        "1": { id: 1, text: 'completed todo', completed: true },
-        "2": { id: 2, text: 'incompleted todo', completed: false }
-      }
-    };
-
-    it('filters all todos', () => {
       expect(getVisibleTodos(state, 'all')).toEqual([
         { id: 1, text: 'completed todo', completed: true },
         { id: 2, text: 'incompleted todo', completed: false }
@@ -73,12 +20,28 @@ describe('todos reducer', () => {
     });
 
     it('filters completed todos', () => {
+      state = {
+        idsByFilter: { completed: [1] },
+        byId: {
+          "1": { id: 1, text: 'completed todo', completed: true },
+          "2": { id: 2, text: 'incompleted todo', completed: false }
+        }
+      };
+
       expect(getVisibleTodos(state, 'completed')).toEqual([
         { id: 1, text: 'completed todo', completed: true }
       ]);
     });
 
     it('filters incompleted todos', () => {
+      state = {
+        idsByFilter: { incompleted: [2] },
+        byId: {
+          "1": { id: 1, text: 'completed todo', completed: true },
+          "2": { id: 2, text: 'incompleted todo', completed: false }
+        }
+      };
+
       expect(getVisibleTodos(state, 'incompleted')).toEqual([
         { id: 2, text: 'incompleted todo', completed: false }
       ]);
