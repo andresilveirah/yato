16470be4437a897ebecdda0702c6c4f9764d6f18
André Herculano <andresilveirah@gmail.com>
2017-04-28 20:14:15 +0200

    enable redux_dev_tools by default if available

--- src/services/configStore.js
+++ src/services/configStore.js
@@ -4,9 +4,14 @@ import reducers from '../reducers/';
 
 const configStore = () => {
   const middlewares = [ thunk ];
+  const enableReduxDevTools = () =>
+      typeof window !== 'undefined'
+        && window.__REDUX_DEVTOOLS_EXTENSION__
+        && window.__REDUX_DEVTOOLS_EXTENSION__();
 
   return createStore(
     reducers,
+    enableReduxDevTools(),
     applyMiddleware(...middlewares)
   );
 };
