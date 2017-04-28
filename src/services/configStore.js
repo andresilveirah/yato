c45e747bf12b22f1b2b3be4c25fa4f7b25f839bd
André Herculano <andresilveirah@gmail.com>
2017-04-28 20:13:49 +0200

    remove redux-logger

--- src/services/configStore.js
+++ src/services/configStore.js
@@ -1,16 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import reducers from '../reducers/';
 
 const configStore = () => {
   const middlewares = [ thunk ];
-
-  // GOTCHA: create-react-app only exposes env variables which are prefixed with REACT_APP_
-  // this is to avoid security issues e.g. exposing sensible env variables in the front end
-  if(process.env.REACT_APP_DEBUG === 'true') {
-    middlewares.push(logger);
-  }
 
   return createStore(
     reducers,
