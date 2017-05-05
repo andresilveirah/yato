3c874f7262bfbdfaa7be6a7f525220ed21430339
Andre Herculano <aherculano@babbel.com>
2017-05-05 17:05:07 +0200

    add styling for the Root and TodoApp components

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,22 +1,12 @@
-/**
- * @module components/TodoApp
- */
-
 import React from 'react';
 
 import Footer from './Footer';
 import AddTodo from '../containers/AddTodo';
 import VisibleTodosList from '../containers/VisibleTodosList';
+import './TodoApp.css';
 
-/**
- * Presentation component for the TodoApp
- * @see {@link module:containers/AddTodo},
- * {@link module:containers/VisibleTodosList} and
- * {@link module:components/Footer}
- * @return {Object} the TodoApp presentation component.
- */
 const TodoApp = () => (
-  <div>
+  <div className='TodoApp'>
     <AddTodo />
     <VisibleTodosList />
     <Footer />
