918f4ae4be95c63ea5983428c6d2fc4c248008af
André Herculano <andresilveirah@gmail.com>
2017-05-06 19:17:03 +0200

    add a onDeleteTodo function on Todo and TodoLists components

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -8,21 +8,23 @@ const onArchiveShortcutPress = (handler, event) => {
   if(event.keyCode === ARCHIVE_SHORTCUT_KEY_CODE) handler(event);
 };
 
-const Todo = ({text, completed, onClick}) => (
+const Todo = ({text, completed, onClick, onDeleteClick}) => (
   <li
     className={completed ? 'TodoList_Item-completed' : 'TodoList_Item'}
     onClick={onClick}
     onKeyDown={onArchiveShortcutPress.bind(null, onClick)}
     role="button"
     tabIndex="0">
-    {text}
+    <span>{text}</span>
+    <button onClick={onDeleteClick} style={{color: 'red'}}>X</button>
   </li>
 );
 
 Todo.propTypes = {
   text: PropTypes.string.isRequired,
   completed: PropTypes.bool.isRequired,
-  onClick: PropTypes.func.isRequired
+  onClick: PropTypes.func.isRequired,
+  onDeleteClick: PropTypes.func.isRequired
 };
 
 export default Todo;
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,27 +1,25 @@
-/**
- * @module components/TodosList
- */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import Todo from './Todo';
 import './TodosList.css';
 
-const TodosList = ({todos, onTodoClick}) => (
+const TodosList = ({todos, onTodoClick, onDeleteClick}) => (
   <ul className='TodosList'>
     {todos.map(todo =>
       <Todo
         key={todo.id}
         text={todo.text}
         completed={todo.completed}
-        onClick={() => { onTodoClick(todo.id); }} />
+        onClick={() => { onTodoClick(todo.id); }}
+        onDeleteClick={() => { onDeleteClick(todo.id); }} />
     )}
   </ul>
 );
 
 TodosList.propTypes = {
   todos: PropTypes.array.isRequired,
-  onTodoClick: PropTypes.func.isRequired
+  onTodoClick: PropTypes.func.isRequired,
+  onDeleteClick: PropTypes.func.isRequired
 };
 
 export default TodosList;
