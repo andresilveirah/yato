539e997a6e663d38fab8a6c2eb2af527853aeaf9
Andre Herculano <aherculano@babbel.com>
2017-05-08 15:35:03 +0200

    add transitions for Todo entering and leaving

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
+
 import Todo from './Todo';
+
 import './TodosList.css';
+
+const renderTodo = (todo, onTodoClick, onDeleteClick) =>
+  <Todo
+    key={todo.id}
+    text={todo.text}
+    completed={todo.completed}
+    onClick={() => { onTodoClick(todo.id); }}
+    onDeleteClick={() => { onDeleteClick(todo.id); }} />;
 
 const TodosList = ({todos, onTodoClick, onDeleteClick}) => (
   <ul className='TodosList'>
-    {todos.map(todo =>
-      <Todo
-        key={todo.id}
-        text={todo.text}
-        completed={todo.completed}
-        onClick={() => { onTodoClick(todo.id); }}
-        onDeleteClick={() => { onDeleteClick(todo.id); }} />
-    )}
+    <CSSTransitionGroup transitionName="TodosList" transitionEnterTimeout={500} transitionLeaveTimeout={300}>
+      { todos.map(todo => renderTodo(todo, onTodoClick, onDeleteClick)) }
+    </CSSTransitionGroup>
   </ul>
 );
 
