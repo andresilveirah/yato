5b63a3d02f4bdc5e3eba6c186e0d9b4de305e403
André Herculano <andresilveirah@gmail.com>
2017-05-09 10:12:04 +0200

    change Roots title based on device size

--- src/components/Root.js
+++ src/components/Root.js
@@ -2,13 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import TodoApp from './TodoApp';
+
 import './Root.css';
 
 const Root = ({ store }) => (
   <div className='Root'>
     <header>
-      <h1 className='Root_Title'>Yet another TODO app</h1>
+      <h1 className='Root_Title'>YATO</h1>
+      <h1 className='Root_Title-Big'>Yet another TODO app</h1>
     </header>
     <Provider store={store}>
       <Router basename='/yato'>
